0546ef94062e1497b3d153275060b9a53bf68122
田茂林 <tian.maolin@zyfax.cn>
2019-07-19 17:25:21 +0800

    springboot项目训练

--- src/main/java/com/tml/SpringBootWebApplication.java
+++ src/main/java/com/tml/SpringBootWebApplication.java
@@ -2,19 +2,12 @@ package com.tml;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootWebApplication extends SpringBootServletInitializer {
+public class SpringBootWebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWebApplication.class, args);
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringBootWebApplication.class);
 	}
 
 }
